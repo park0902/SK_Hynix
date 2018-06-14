928c1e5dd7b5290a0eefdf034d039e0e34dcab4e
park0902 <sjm981690418!>
2018-06-14 22:05:31 +0900

    2018/06/14

--- model/abstract/Classifier.py
+++ model/abstract/Classifier.py
@@ -1,20 +1,16 @@
 import hyperopt
-# from scipy import stats
-# from sklearn.metrics import log_loss
+import time
+from sklearn.metrics import log_loss
+from sklearn.model_selection import train_test_split
 import numpy as np
-# from model.abstract.Model import Model
 from model.abstract import Model
-# import model.abstract.Model
 
-
-# import pandas as pd
-# from sklearn import preprocessing, svm
-
-# from sklearn import preprocessing, svm
 
 class Classifier(Model):
+    def __init__(self):
+        super(Classifier, self).__init__()
 
-    def evaluate(self, X_test, y_test):
+    def evaluate(self, data, labels):
         """
 
         :param X_test:
@@ -24,10 +20,10 @@ class Classifier(Model):
         :return:
         """
 
-        y_predict = self.predict(X_test)
+        predict_lables = self.predict(data)
         TP = 0; FP = 0; TN = 0; FN = 0;
-        for i, y_t in enumerate(y_test):
-            y_p = y_predict[i]
+        for i, y_t in enumerate(predict_lables):
+            y_p = predict_lables[i]
             if y_p == 0 and y_t==0 : # TP
                 TP +=1
             elif y_p == 0 and y_t==1 : # FP
@@ -36,31 +32,47 @@ class Classifier(Model):
                 TN += 1
             elif y_p == 1 and y_t==0 : # FN
                 FN += 1
-        return self.binary_evaluate(TP, FP, TN, FN, y_test, y_predict, pos_label=0), y_predict
+        return self.binary_evaluate(TP, FP, TN, FN, labels, predict_lables, pos_label=0), y_predict
 
     @classmethod
-    def _get_tune(cls, data, labels, params={}, cv=10):
+    def _get_tune(cls, data, labels, cv=10):
         def tune(params):
+            tmp_model = cls()
+            start_time = time.time()
             status = hyperopt.STATUS_OK
             try:
-                evl_list, y_predict_list = cls.cross_val(data, labels=labels, params=params, cv=cv)
-                loss = np.mean([evl['loss'] for evl in evl_list])
-                print('loss ================================>', loss)
-                evl = dict()
-                evl['TP'] = np.rint(np.mean([evl['TP'] for evl in evl_list]))
-                evl['FP'] = np.rint(np.mean([evl['FP'] for evl in evl_list]))
-                evl['TN'] = np.rint(np.mean([evl['TN'] for evl in evl_list]))
-                evl['FN'] = np.rint(np.mean([evl['FN'] for evl in evl_list]))
-                evl['acc'] = np.mean([evl['acc'] for evl in evl_list])
-                evl['err'] = np.mean([evl['err'] for evl in evl_list])
-                evl['precision'] = np.mean([evl['precision'] for evl in evl_list])
-                evl['recall'] = np.mean([evl['recall'] for evl in evl_list])
-                evl['f1'] = np.mean([evl['f1'] for evl in evl_list])
+                if cv <= 1:
+                    X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=1)
+                    tmp_model.train(X_train, labels=y_train, params=params)
+                    eval_results, y_predict = tmp_model.evaluate(X_test, y_test)
+                    loss = log_loss(y_test, y_predict)
+                    print('loss', loss)
+                else:
+                    eval_list, y_predict_list = cls.cross_val(data, labels=labels, params=params, cv=cv)
+                    loss = np.array([evl['loss'] for evl in evl_list]).mean()
+                    print('loss ================================>', loss)
+                    eval_results = {"TP" : np.rint(np.array([evl_['TP'] for evl in eval_list]).mean()}
+
+
+
+
+
+                    evl['FP'] = np.rint(np.mean([evl['FP'] for evl in evl_list])),
+                    evl['TN'] = np.rint(np.mean([evl['TN'] for evl in evl_list])),
+                    evl['FN'] = np.rint(np.mean([evl['FN'] for evl in evl_list])),
+                    evl['acc'] = np.mean([evl['acc'] for evl in evl_list]),
+                    evl['err'] = np.mean([evl['err'] for evl in evl_list]),
+                    evl['precision'] = np.mean([evl['precision'] for evl in evl_list]),
+                    evl['recall'] = np.mean([evl['recall'] for evl in evl_list]),
+                    evl['f1'] = np.mean([evl['f1'] for evl in evl_list])
+                    }
+                exe_time = time.time() - start_time
             except Exception as e:
                 print('Error', e)
-                loss = 10000000
+                loss = 1000000000000000000
                 status = hyperopt.STATUS_FAIL
-                evl = {}
+                eval_results = {}
+                exe_time = 0
             finally:
                 return {'loss': loss, 'status': status, 'eval': evl}
             # evl_list, y_predict_list = cls.cross_val(data, labels=labels, params=params, cv=cv)
@@ -81,13 +93,14 @@ class Classifier(Model):
 
 
     @classmethod
-    def tuning(cls, data, labels,  space, max_evals, params={}, mode='tpe', cv=10):
-        if mode == 'tpe':
-            algo = hyperopt.tpe.suggest
-        elif mode == 'random':
-            algo = hyperopt.rand.suggest
-        tune = cls._get_tune(data, labels, params=params, cv=cv)
+    def tuning(cls, data, labels,  space, max_evals, tune_algo='tpe', cv=10):
+        tune = cls._get_tune(data, labels, cv=cv)
         trials = hyperopt.Trials()
+        if tune_algo == 'tpe':
+            algo = hyperopt.tpe.suggest
+        elif tune_algo == 'random':
+            algo = hyperopt.rand.suggest
+
         best = hyperopt.fmin(tune, space, algo=algo, max_evals=max_evals, trials=trials)
         return best, trials
 
--- model/abstract/Model.py
+++ model/abstract/Model.py
@@ -1,71 +1,30 @@
 import time
 from abc import *
+from sklearn.externals import joblib
+from sklearn.model_selection import StratifiedKFold
+from sklearn import metrics
 
-import sklearn.externals.joblib
-import sklearn.metrics
-import sklearn.model_selection
-
-
-# from sklearn.model_selection import KFold
-# from sklearn.metrics import log_loss
-# from scipy import stats
-# import numpy as np
-# import pandas as pd
-# from sklearn import preprocessing, svm
-#
-# import pickle
-# import os
-#
-# from sklearn import preprocessing, svm
 
 class Model(metaclass=ABCMeta):
 
-    # @staticmethod
-    # def _get_label_encoder(encoder_path):
-    #     if os.path.exists(encoder_path):
-    #         with open(encoder_path, 'rb') as f:
-    #             return pickle.load(f)
-    #     else:
-    #         return None
-    #
-    # @staticmethod
-    # def _make_label_encoder(encoder_path, labels_value):
-    #     encoder = preprocessing.LabelEncoder()
-    #     encoder.fit(labels_value)
-    #     with open(encoder_path, 'wb') as f:
-    #         pickle.dump(encoder, f)
-    #     return encoder
-    #
-    # def _get_label(self, label_col):
-    #     label_df = self.data_df[label_col]
-    #     encoder_path = './col_%s.pkl'
-    #     encoder = self._get_label_encoder(encoder_path)
-    #     if encoder is None:
-    #         encoder = self._make_label_encoder(encoder_path, label_df.values)
-    #     encoder.fit(label_df.values)
-    #     labels = encoder.transform(label_df.values)
-    #     return labels
-    #
-    # @abstractmethod
-    # def load_data(self, file_path, num_cols, cate_cols, label_col='', **kwargs):
-    #
-    #     pass
+    def __init__(self):
+        pass
 
     @abstractmethod
-    def train(self, data, labels=None, params={}):
+    def train(self, data, labels=None, params={}, **kwargs):
         pass
 
     def predict(self, data):
         return self.model.predict(data)
 
     def save_model(self, file_path):
-        sklearn.externals.joblib.dump(self.model, file_path)
+        joblib.dump(self.model, file_path)
 
     def load_model(self, file_path):
-        self.model = sklearn.externals.joblib.load(file_path)
+        self.model = joblib.load(file_path)
 
     @staticmethod
-    def binary_evaluate(TP, FP, TN, FN, y_true, y_pred, pos_label=0):
+    def binary_clf_evaluate(TP, FP, TN, FN, y_true, y_pred, pos_label=0):
         """
 
         :param TP:
@@ -79,8 +38,8 @@ class Model(metaclass=ABCMeta):
         """
         result ={'TP': TP, 'FP': FP, 'TN': TN, 'FN': FN}
         try:
-            fpr, tpr, threshold = sklearn.metrics.roc_curve(y_true, y_pred, pos_label=pos_label)
-            result['auc'] = sklearn.metrics.auc(fpr, tpr)
+            fpr, tpr, threshold = metrics.roc_curve(y_true, y_pred, pos_label=pos_label)
+            result['auc'] = metrics.auc(fpr, tpr)
             result['acc'] = (TP + TN) / (TP + FP + TN + FN)
             result['err'] = (FP + FN) / (TP + FP + TN + FN)
 
@@ -109,7 +68,7 @@ class Model(metaclass=ABCMeta):
 
     @classmethod
     def cross_val(cls, data, labels=[], params={}, cv=10, **kwargs): # SVDD 는 안되겄다.
-        kf = sklearn.model_selection.StratifiedKFold(n_splits=cv, random_state=1, shuffle=True)
+        kf = StratifiedKFold(n_splits=cv, random_state=1, shuffle=True)
         evl_list =[]
         y_predict_list = []
         for train_index, test_index in kf.split(data, labels):
@@ -125,21 +84,11 @@ class Model(metaclass=ABCMeta):
             else:
                 model.train(X_train, y_train, params=params)
 
-            evl, y_predict = model.evaluate(X_test, y_test)
-            evl['loss'] = sklearn.metrics.log_loss(y_test, y_predict)
-            evl['exe_time'] = time.time() - start_time
-            evl_list.append(evl)
+            eval_result, y_predict = model.evaluate(X_test, y_test)
+            eval_result['loss'] = metrics.log_loss(y_test, y_predict)
+            eval_result['exe_time'] = time.time() - start_time
+            evl_list.append(eval_result)
             y_predict_list.append(y_predict)
         print('evl_list ================>', evl_list)
         return evl_list, y_predict_list
-
-
-    # @abstractmethod
-    # def save_model(self, data):
-    #     pass
-    #
-    # @abstractmethod
-    # def load_model(self, data):
-    #     pass
-
 
